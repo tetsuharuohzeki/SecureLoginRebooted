0ed3b87e54118d8bec8d3a4d7dd88d083fc347fe
saneyuki_s <saneyuki.s.snyk@gmail.com>
2011-12-15 23:51:54 +0900

    Change a comment style.

--- defaults/preferences/securelogin.js
+++ defaults/preferences/securelogin.js
@@ -35,16 +35,23 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-// Use protected login
+/**
+ * Use the protected login.
+ */
 pref("extensions.securelogin.loginWithProtection", true);
 
-// Override the login form action & method if the login form's action or method
-// are overwritten by someone, when using normal login.
+/**
+ * Override the login form action & method
+ * if the login form's action or method are overwritten by someone,
+ * when using normal login.
+ */
 pref("extensions.securelogin.overrideFormAction", true);
 
-// Change Firefox default setting.
-// Set password to form if this pref is true.
-// However this behavior is not secure completely.
-// If a page has XSS risk,
-// it's possible that an evil person steals the password.
+/**
+ * Change Firefox default setting.
+ * Set password to form if this pref is true.
+ * However this behavior is not secure completely.
+ * If a page has XSS risk,
+ * it's possible that an evil person steals the password.
+ */
 pref("signon.autofillForms", false);
